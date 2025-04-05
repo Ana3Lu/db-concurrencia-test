9e5b06019eb26d5d0ba78597280daf20101d85b0
Ana3Lu <anaquiva@unisabana.edu.co>
2025-04-05 00:03:40 -0500

    Agrega manejo de variables de entorno

--- src/main/java/co/edu/unisabana/banco/logica/TransaccionService.java
+++ src/main/java/co/edu/unisabana/banco/logica/TransaccionService.java
@@ -4,11 +4,16 @@ import co.edu.unisabana.banco.db.jpa.CuentaJPA;
 import co.edu.unisabana.banco.db.jpa.TransaccionJPA;
 import co.edu.unisabana.banco.db.orm.CuentaORM;
 import co.edu.unisabana.banco.db.orm.TransaccionORM;
+import jakarta.persistence.OptimisticLockException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
+import org.springframework.retry.annotation.Retryable;
+
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
@@ -17,15 +22,20 @@ public class TransaccionService {
     private CuentaJPA cuentaJPA;
     private TransaccionJPA transaccionJPA;
 
+    @Retryable(
+            value = OptimisticLockException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 100) // Espera 100ms antes de reintentar
+    )
     @Transactional
     public void moverDinero(Long origenId, Long destinoId, float monto) {
         CuentaORM origen = cuentaJPA.findById(origenId)
-                .orElseThrow(() -> new RuntimeException("Cuenta origen no encontrada"));
+                .orElseThrow(() -> new NoSuchElementException("Cuenta origen no encontrada"));
         CuentaORM destino = cuentaJPA.findById(destinoId)
-                .orElseThrow(() -> new RuntimeException("Cuenta destino no encontrada"));
+                .orElseThrow(() -> new NoSuchElementException("Cuenta destino no encontrada"));
 
         if (origen.getMonto() < monto) {
-            throw new RuntimeException("Saldo insuficiente");
+            throw new IllegalStateException("Monto insuficiente en la cuenta origen");
         }
 
         origen.setMonto(origen.getMonto() - monto);
